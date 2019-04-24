2a28861afa5e93f8d1ffb90e3802d14e4092f450
Jonas Laursen <joamla96@gmail.com>
2019-04-24 11:21:05 +0200

    Moved implementation to seperate libary

--- MathLibary/PiCalculator.cs
+++ MathLibary/PiCalculator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PiCalculator_Classic
+namespace MathLibary
 {
     public class PiCalculator
     {
---
+++ PiCalculator/Values.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using MathLibary;
+
+namespace PiCalculator
+{
+    public static class Values
+    {
+        [FunctionName("GetPi")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string decimalString = req.Query["decimals"];
+
+            if(string.IsNullOrEmpty(decimalString)) {
+                return new BadRequestObjectResult("Missing Decimals");
+            }
+
+            int digits;
+            if(!int.TryParse(decimalString, out digits)) {
+                return new BadRequestObjectResult("Invalid decimal parameter");
+            }
+
+            var pi = await Task.Run(() => MathLibary.PiCalculator.GetPi(digits));
+            return new OkObjectResult(pi);
+        }
+    }
+}
