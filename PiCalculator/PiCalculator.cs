16627f4070f6bdb2fe707ed0a6c7a284cf517f97
Jonas Laursen <joamla96@gmail.com>
2019-04-24 11:12:07 +0200

    Implemented function for calculating pi

--- PiCalculator/PiCalculator.cs
+++ PiCalculator/PiCalculator.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 
 namespace PiCalculator
 {
-    public static class Function1
+    public static class PiCalculator
     {
         [FunctionName("GetPi")]
         public static async Task<IActionResult> Run(
@@ -25,12 +25,57 @@ namespace PiCalculator
                 return new BadRequestObjectResult("Missing Decimals");
             }
 
-            int decimals;
-            if(!int.TryParse(decimalString, out decimals)) {
+            int digits;
+            if(!int.TryParse(decimalString, out digits)) {
                 return new BadRequestObjectResult("Invalid decimal parameter");
             }
 
-            throw new NotImplementedException();
+            digits++;
+
+            uint[] x = new uint[digits * 10 / 3 + 2];
+            uint[] r = new uint[digits * 10 / 3 + 2];
+
+            uint[] pi = new uint[digits];
+
+            for (int j = 0; j < x.Length; j++)
+                x[j] = 20;
+
+            for (int i = 0; i < digits; i++) {
+                uint carry = 0;
+                for (int j = 0; j < x.Length; j++) {
+                    uint num = (uint)(x.Length - j - 1);
+                    uint dem = num * 2 + 1;
+
+                    x[j] += carry;
+
+                    uint q = x[j] / dem;
+                    r[j] = x[j] % dem;
+
+                    carry = q * num;
+                }
+
+
+                pi[i] = (x[x.Length - 1] / 10);
+
+
+                r[x.Length - 1] = x[x.Length - 1] % 10; ;
+
+                for (int j = 0; j < x.Length; j++)
+                    x[j] = r[j] * 10;
+            }
+
+            var result = "";
+
+            uint c = 0;
+
+            for (int i = pi.Length - 1; i >= 0; i--) {
+                pi[i] += c;
+                c = pi[i] / 10;
+
+                result = (pi[i] % 10).ToString() + result;
+            }
+
+            return new OkObjectResult(result);
         }
     }
 }
