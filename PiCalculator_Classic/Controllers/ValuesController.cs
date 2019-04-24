964ed5cf90b9a85a0296445b89ff5b4797d4fc06
Jonas Laursen <joamla96@gmail.com>
2019-04-24 11:15:42 +0200

    Implemented pi calculator for "classic" application

--- PiCalculator_Classic/Controllers/ValuesController.cs
+++ PiCalculator_Classic/Controllers/ValuesController.cs
@@ -10,31 +10,20 @@ namespace PiCalculator_Classic.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get() {
-            return new string[] { "value1", "value2" };
-        }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id) {
-            return "value";
+        [HttpGet("{digits}")]
+        public async Task<IActionResult> Get(int digits) {
+            var pi = await Task.Run(() => PiCalculator.GetPi(digits));
+            return Ok(pi);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value) {
+        public async Task<IActionResult> Post([FromBody] int digits) {
+            var pi = await Task.Run(() => PiCalculator.GetPi(digits));
+            return Ok(pi);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value) {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id) {
-        }
     }
 }
