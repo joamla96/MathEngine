31f1973c4aa925f6950055b6c54df1aed45de501
Jonas Laursen <joamla96@gmail.com>
2019-04-24 12:44:20 +0200

    MAKE auth anon

--- PiCalculator/Values.cs
+++ PiCalculator/Values.cs
@@ -15,7 +15,7 @@ namespace PiCalculator
     {
         [FunctionName("GetPi")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
