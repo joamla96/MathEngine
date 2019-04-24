0b9bfb1bfc97788239f2c585b87c071be31afb4b
Jonas Laursen <joamla96@gmail.com>
2019-04-24 11:05:50 +0200

    Prepared pi calculator function

--- PiCalculator/PiCalculator.cs
+++ PiCalculator/PiCalculator.cs
@@ -12,22 +12,25 @@ namespace PiCalculator
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("GetPi")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string decimalString = req.Query["decimals"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            if(string.IsNullOrEmpty(decimalString)) {
+                return new BadRequestObjectResult("Missing Decimals");
+            }
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            int decimals;
+            if(!int.TryParse(decimalString, out decimals)) {
+                return new BadRequestObjectResult("Invalid decimal parameter");
+            }
+
+            
         }
     }
 }
